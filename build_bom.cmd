f670a727632f529347193dd203a0facc1eba7b27
donghee <dongheepark@gmail.com>
2018-07-22 20:52:54 +0900

    Add schematic and board, bom files

---
+++ build_bom.cmd
@@ -0,0 +1,1 @@
+c:\Python27\python.exe c:\Python27\Scripts\kicost --overwrite -i maru-x21.xml
---
+++ build_bom.sh
@@ -0,0 +1,3 @@
+kicost --overwrite -i maru-x21.xml --include digikey 
+#kicost --overwrite -i maru-pixracer.xml --include digikey mouser
+#kicost digikey.com -> digikey.kr
